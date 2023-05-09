14f10ac7b6820d2b370ae3344f7bbfdec8acd911
Cariny <perezcariny@gmail.com>
2023-05-09 15:46:49 -0700

    answers for week 9

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express();
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 
+// extra security packages 
+const helmet = require('helmet');
+const cors = require('cors'); 
+const xss  = require('xss-clean'); 
+const rateLimiter = require('express-rate-limit');
+
+
 // routers
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
@@ -13,6 +20,15 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.set('trust proxy', 1);
+// limit each IP to 100 requests per windowMs 
+app.use(rateLimiter({
+	windowsMs: 15 * 60 * 1000,
+	max: 100,
+}))
 app.use(express.json());
 // extra packages
 
@@ -37,4 +53,5 @@ const start = async () => {
 };
 
 start();
+
 
